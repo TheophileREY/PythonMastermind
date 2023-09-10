d441265f10e00b856ce5664213aa1ff5797dce84
TheophileREY <theophile.rey@wanadoo.fr>
2023-09-10 15:33:30 +0200

    Version Modularite

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ couleurs = {
     'N': 'Noir'
     }
 
+# Nombre max d'essai avant l'echec du joueur
+essai_max = 12
+
+#Nombre de couleur du code secret
+nbr_couleur = 5
+
 # Générer le code secret
 code_secret = ''
-for i in range(4):
+for i in range(nbr_couleur):
     code_secret += random.choice(list(couleurs.keys())) # On choisit un code au hasard dans le dictionnaire des couleurs
 
 # Afficher les regles et les couleurs possibles
@@ -25,13 +31,13 @@ for code, couleur in couleurs.items():
 # Initialiser le nombre d'essais effectue par le joueur
 essais = 0
 
-while essais < 12:
+while essais < essai_max:
     essais += 1
-    print("Essai #" + str(essais) + ": Entrez le code (ex. 'RVBJ'):")
+    print("Essai #" + str(essais) + ": Entrez le code a " + str(nbr_couleur) + " couleurs (Exemple pour 4 : 'RVBJ'):")
     reponse = input().upper() # Pour mettre la reponse en MAJ dans tout les cas
 
     # Vérifier la validité de la réponse (longueur)
-    if len(reponse) != 4:
+    if len(reponse) != nbr_couleur:
         print("Erreur : Le code entre possede plus de 4 lettres")
         continue
 
@@ -49,7 +55,7 @@ while essais < 12:
     # Vérifier si la réponse est correcte
     correct = 0
     partiel = 0
-    for i in range(4):
+    for i in range(nbr_couleur):
         if reponse[i] == code_secret[i]:
             correct += 1
         elif reponse[i] in code_secret:
@@ -59,10 +65,10 @@ while essais < 12:
     print("Correct : " + str(correct) + " | Partiel : " + str(partiel))
 
     # Vérifier si le joueur a gagne
-    if correct == 4:
+    if correct == nbr_couleur:
         print("Felicitations, vous avez trouve le code secret !")
         break
 
 # Afficher le score
-score = 12 - essais
+score = essai_max - essais
 print("Votre score est : " + str(score))
